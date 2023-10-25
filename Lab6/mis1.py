5a07d2a76520d29bea7caa34878233e4ba08dcb4
Abdyukov Zufar <swat55551@gmail.com>
2023-10-25 20:24:36 +0300

    Add theory

--- Lab6/mis1.py
+++ Lab6/mis1.py
@@ -12,29 +12,40 @@ if __name__ == '__main__':
     (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
     # Преобразование данных
-    train_images = train_images.reshape((60000, 28, 28, 1))
-    train_images = train_images.astype('float32') / 255
+    train_images = train_images.reshape((60000, 28, 28, 1)) #кол картинок, XxY, Кол каналов 1 - серый
+    train_images = train_images.astype('float32') / 255 #нормализацию данных, разделяя все значения на 255. для диап 0-1
 
     test_images = test_images.reshape((10000, 28, 28, 1))
     test_images = test_images.astype('float32') / 255
 
+    #преобразовать метки классов в one-hot кодировку.
+    #One-hot кодировка - это способ представления категориальных данных, таких как метки классов, в виде бинарных векторов
     train_labels = to_categorical(train_labels)
     test_labels = to_categorical(test_labels)
 
     model = Sequential() #последовательный стек слоев нейронов.
+
     model.add(Flatten(input_shape=(28, 28, 1))) #добавляет первый слой к модели.
-    '''Слой Flatten предназначен для преобразования входных данных в одномерный вектор. В данном случае, input_shape=(28, 28, 1) указывает, что входные данные представляют собой изображения размером 28x28 пикселей с одним каналом (черно-белое изображение).'''
+    '''Слой Flatten предназначен для преобразования входных данных в одномерный вектор.'''
     model.add(Dense(128, activation='relu'))
-    '''добавляется второй слой к модели. Это полносвязный (Dense) слой с 128 нейронами. activation='relu' указывает, что активационная функция этого слоя - ReLU (Rectified Linear Unit). Функция ReLU широко используется в нейронных сетях для введения нелинейности.'''
+    '''Это полносвязный (Dense) слой с 128 нейронами.Функция ReLU широко используется в нейронных сетях для введения нелинейности.'''
     model.add(Dense(64, activation='relu'))
-    model.add(Dense(10, activation='softmax'))
-    '''Последний слой является выходным слоем. Он также полносвязный, но имеет 10 нейронов, соответствующих 10 классам (цифры от 0 до 9), и использует активационную функцию softmax. Слой softmax используется для многоклассовой классификации и выводит вероятности принадлежности объекта к каждому из классов.'''
+
+    model.add(Dense(10, activation='softmax'))#exit
+    '''Он также полносвязный, но имеет 10 нейронов, соответствующих 10 классам (цифры от 0 до 9) Слой softmax используется для многоклассовой классификации и выводит вероятности принадлежности объекта к каждому из классов.'''
 
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy']) #Скомпилируйте модель, указав функцию потерь и оптимизатор.
     model.fit(train_images, train_labels, epochs=5, batch_size=64) #Обучите модель на обучающих данных
 
     test_loss, test_acc = model.evaluate(test_images, test_labels)
     print('Test accuracy:', test_acc)
+''' Последовательное обучение
+    for i in range(len(train_images)):
+        image = train_images[i:i + 1]  # Выбираем одно изображение
+        label = train_labels[i:i + 1]  # Выбираем соответствующую метку
+
+        # Обучаем модель на одном образце
+        model.train_on_batch(image, label)'''
 
 '''
 Optimizer
@@ -46,7 +57,7 @@ optimizer = keras.optimizers.Adagrad(learning_rate=0.01)
 '''
 
 '''
-loss='categorical_crossentropy': Это функция потерь (или loss function), которая используется для измерения ошибки между предсказаниями модели и истинными метками (целями) во время обучения. 'categorical_crossentropy' обычно используется в многоклассовой классификации, где каждому объекту можно присвоить несколько классов. 
+loss='categorical_crossentropy': Это функция потерь ,  используется для измерения ошибки между предсказаниями модели и верными ответами во время обучения. 'categorical_crossentropy' обычно используется в многоклассовой классификации, где каждому объекту можно присвоить несколько классов. 
 '''
 
 '''
--- Lab6/mis2.py
+++ Lab6/mis2.py
@@ -10,10 +10,8 @@ from keras.utils import to_categorical
 
 
 if __name__ == '__main__':
-    # Загрузка данных MNIST
     (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-    # Преобразование данных
     train_images = train_images.reshape((60000, 28, 28, 1))
     train_images = train_images.astype('float32') / 255
 
@@ -28,26 +26,20 @@ if __name__ == '__main__':
     model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
     '''layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)): Этот код создает сверточный слой (Convolutional Layer). Важные аргументы:
 32: Это количество фильтров, которые этот сверточный слой будет использовать для извлечения признаков из входных данных.
-(3, 3): Это размер фильтра свертки (3x3), который будет скользить по входным данным для выполнения операции свертки.
-activation='relu': Это функция активации, в данном случае, функция ReLU (Rectified Linear Unit), которая используется для введения нелинейности в слой.
-input_shape=(28, 28, 1): Это форма входных данных. В данном случае, ожидается, что входные изображения имеют размер 28x28 пикселей и один канал (градации серого), поскольку последнее число 1 указывает на количество каналов.'''
+(3, 3): Это размер фильтра свертки (3x3), который будет скользить по входным данным для выполнения операции свертки.'''
     model.add(layers.MaxPooling2D((2, 2)))
-    '''Эта строка добавляет слой пулинга (Max Pooling) к модели. Пулинг используется для уменьшения размерности и извлечения наиболее важных признаков из предыдущего сверточного слоя. (2, 2) указывает на размер окна, которое скользит по данным и выбирает максимальное значение.'''
+    '''Эта строка добавляет слой пулинга  к модели. Пулинг используется для уменьшения размерности и извлечения наиболее важных признаков из предыдущего сверточного слоя. (2, 2) указывает на размер окна, которое скользит по данным и выбирает максимальное значение.'''
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
     model.add(layers.Flatten())
-    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(64, activation='relu')) #полносвязный слой
     model.add(layers.Dense(10, activation='softmax'))
 
-    # Компиляция модели
-    model.compile(optimizer='adam',
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
 
-    # Обучение модели
+    model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+
     model.fit(train_images, train_labels, epochs=5, batch_size=64, validation_split=0.2)
-
-    # Оценка модели
+    '''validation_split=0.2:  20% обучающих данных будут использованы для валидации в каждой эпохе обучения. Это помогает отслеживать производительность модели на данных, которые не использовались в обучении.'''
     test_loss, test_acc = model.evaluate(test_images, test_labels)
     print(f'Точность на тестовых данных: {test_acc}')
